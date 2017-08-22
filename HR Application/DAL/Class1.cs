b253c7bb852df805d89a4af397c5e0ab180ad165
Dinesh Nanayakkara <dineshucsc@gmail.com>
2017-08-22 11:24:55 +0530

    Add database Connection string
    create database string in DAL folder

---
+++ HR Application/DAL/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace HR_Application.DAL
+{
+    class DbConnect
+    {
+        string conn_string = "Data Source=CMBTRNDB02/APP8SP2;User ID=ifsapp;Password=ifsapp;Unicode=True";
+
+
+    }
+}
