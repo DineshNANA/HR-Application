236ded94cfa234a74e2777a64165e1af016553b4
Dinesh Nanayakkara <dineshucsc@gmail.com>
2017-08-23 10:51:09 +0530

    register employee with auto increment
    register employee with auto increment

--- register_employee_procedure.sql
+++ register_employee_procedure.sql
@@ -1,11 +1,33 @@
-CREATE OR REPLACE PROCEDURE REGISTER_EMPLOYEE (emp_id IN VARCHAR2,emp_name IN VARCHAR2,nic_no IN VARCHAR2,address IN VARCHAR2,contact_no IN VARCHAR2, designation IN VARCHAR2)
-IS 
-BEGIN 
-  INSERT INTO HR_EMPLOYEE VALUES(emp_id,emp_name,nic_no,address,contact_no,designation);
-END REGISTER_EMPLOYEE;
+CREATE OR REPLACE PROCEDURE register_employee(
+       
+       emp_name IN VARCHAR2, 
+       nic_no IN VARCHAR2, 
+       address IN VARCHAR2, 
+       contact_no IN VARCHAR2, 
+       designation IN VARCHAR2, 
+       dep_id IN VARCHAR2) 
+IS
 
+emp_ide VARCHAR2(3);
 
+BEGIN
+       emp_ide := TO_CHAR(HR_EMP_ID.nextval);
+       INSERT INTO HR_Employee VALUES(
+       'EMP'||emp_ide, 
+       emp_name, 
+       nic_no, 
+       address, 
+       contact_no, 
+       designation, 
+       dep_id);
+       
+
+END register_employee;
+
+/*create sequence HR_EMP_ID NOCACHE start with 101;
+
+select * from HR_EMPLOYEE
 BEGIN 
-  REGISTER_EMPLOYEE('003','John Smith','864125698V','Colombo','0742674289','labor');
-  commit;
-END;
+  register_employee('Dinesh Nanayakkara','931413584V','Galle','0742514963','Manager','DEP1');
+end;
+*/
