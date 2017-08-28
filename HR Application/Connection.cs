8b9a6d1f31a58f098b414032334264dea1090fb5
Mohamed Afrar <mmafrar@gmail.com>
2017-08-28 12:49:31 +0530

    Added a class to handle DB connection

---
+++ HR Application/Connection.cs
@@ -0,0 +1,15 @@
+﻿using Oracle.DataAccess.Client;
+
+namespace HR_Application
+{
+    public static class Connection
+    {
+        static string connectionString = "Data Source=CMBTRNDB02/APP8SP2;User ID=ifsapp;Password=ifsapp";
+        static OracleConnection connection = new OracleConnection(connectionString);
+
+        public static OracleConnection Main()
+        {
+            return connection;
+        }
+    }
+}
