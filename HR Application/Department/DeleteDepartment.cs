2d16f14aeb0ce6eb6fda54a586cb9e837e45a601
Mohamed Afrar <mmafrar@gmail.com>
2017-08-29 12:22:47 +0530

    Added functionality to delete Department

---
+++ HR Application/Department/DeleteDepartment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace HR_Application
+{
+    public partial class DeleteDepartment : Form
+    {
+        Department department;
+
+        public DeleteDepartment()
+        {
+            InitializeComponent();
+            department = new Department();
+            ArrayList deptList = department.getDepartmentList();
+
+            foreach(string dept in deptList)
+            {
+                comboBox1.Items.Add(dept);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string depId = comboBox1.Text.Substring(0,6);
+            department.deleteDepartment(depId);
+        }
+    }
+}
